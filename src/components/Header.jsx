638161a6470017a011db1a659373db362402b995
Sebastian Rios <juanriosubeda@gmail.com>
2025-03-25 11:15:02 -0700

    Header done

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <img src="src\assets\Globe.svg" alt="" />
+        <span>my travel journal.</span>
+      </header>
+    </>
+  )
+}
+
+export default Header
