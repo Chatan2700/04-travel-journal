936515dcbc067e5dcdc0d13e0e79a269f672d5ae
Sebastian Rios <juanriosubeda@gmail.com>
2025-03-27 15:45:03 -0700

    Finished the travel jorunal

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import './App.css'
 import Entry from './components/Entry'
 import Header from './components/Header'
+import data from '../data'
 
 function App() {
+
+  const entryComponent = data.map((entry) => {
+    return (
+      <Entry
+        key={entry.id}
+        entry={entry}
+      // {...entry} //Spread operator
+      />
+    )
+  })
 
   return (
     <>
       <div className="container">
         <Header />
-        <Entry />
-        <Entry />
+        {entryComponent}
       </div>
 
     </>
